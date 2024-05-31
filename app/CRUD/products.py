87375622891cf309199c764924dbc6baff605d95
Akere Aishat <mojiradeaisha850@gmail.com>
2024-05-31 17:16:12 +0100

    update

--- app/CRUD/products.py
+++ app/CRUD/products.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, HTTPException, status, APIRouter
+from fastapi import  Depends, HTTPException, status, APIRouter
 from .. import schema, model, security
 from ..database import get_db
 from sqlalchemy.orm import session
@@ -18,7 +18,7 @@ def get_post(db: session = Depends(get_db)):
 
 
 @router.post('/', status_code=status.HTTP_201_CREATED)
-def create_poduct(product: schema.Product, db: session = Depends(get_db), current_user: int = Depends(security.get_current_user)):
+def create_poduct(product: schema.Product, db: session = Depends(get_db), current_user: str = Depends(security.get_current_user)):
 
     # print(current_user.id)
     new_product = model.Products(**product.dict())
@@ -30,17 +30,17 @@ def create_poduct(product: schema.Product, db: session = Depends(get_db), curren
 
 
 @router.get('/{id}')  # , response_model= List[schema.Post])
-def quary_by_one(id: int, db: session = Depends(get_db)):
+def quary_by_one(id: int, db: session = Depends(get_db),current_user:str =  Depends(security.get_current_user)):
     product = db.query(model.Products).filter(model.Products.id == id).first()
 
     if not product:
         raise HTTPException(status_code=status.HTTP_204_NO_CONTENT,
-                            detail='there is no post with this post, check again')
+                            detail='the product you are looking for does not exist, check again')
     return {product}
 
 
 @router.delete('/')
-def delete_post(id: int, db: session = Depends(get_db), current_user: dict = Depends(security.get_current_user)):
+def delete_post(id: int, db: session = Depends(get_db), current_user:str  = Depends(security.get_current_user)):
     delee = db.query(model.Products).filter(model.Products.id == id)
     product = delee.first()
     if product == None:
@@ -58,7 +58,7 @@ def updating_post(id: int, product: schema.Product, db: session = Depends(get_db
     product = updte.first()
     if product == None:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail=' post not found')
+            status_code=status.HTTP_404_NOT_FOUND, detail=' product not found')
     updte.update(product.dict(), synchronize_session=False)
     db.commit()
 
